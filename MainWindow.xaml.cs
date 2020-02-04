cf731ce6c17ba00b51501e8884e4b1724623b5a5
nerdhaven <60668123+nerdhaven@users.noreply.github.com>
2020-02-04 13:39:01 -0500

    Added files
    These are the two base xaml and xaml.cs files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184648SecondProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGreeting_Click(object sender, RoutedEventArgs e)
+        {
+            lblGreeting.Content = "Yessss good work " + txtBName.Text + " you will work just fine... Juuuust fine >:)";
+        }
+    }
+}
